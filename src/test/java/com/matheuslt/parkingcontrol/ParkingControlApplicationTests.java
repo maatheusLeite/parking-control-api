397441c7a7e759bb255fd4d46320b225082b3525
Matheus Leite <maatheus2710@gmail.com>
2023-06-05 20:08:30 -0300

    Project Created

---
+++ src/test/java/com/matheuslt/parkingcontrol/ParkingControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuslt.parkingcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
